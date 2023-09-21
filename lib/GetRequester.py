1cf56fbfa3671fc9a1c1dcfb76bc9374baa41182
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-09-21 09:31:31 +0300

    NEw updates

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,18 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()  
+            return response.text
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Failed to fetch data from {self.url}: {str(e)}")
+
 
     def load_json(self):
-        pass
+          response_body = self.get_response_body()
+          try:
+              data = json.loads(response_body)  
+              return data
+          except json.JSONDecodeError as e:
+            raise Exception(f"Failed to parse JSON data from {self.url}: {str(e)}")
